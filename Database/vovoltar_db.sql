872523e9c656587fb38402e111fa7b58ace20412
Thiago <thiagotassinari10@gmail.com>
2024-09-26 00:31:42 -0300

    atualização database

--- Database/vovoltar_db.sql
+++ Database/vovoltar_db.sql
@@ -1,18 +1,24 @@
 CREATE DATABASE vovoltar;
-
 USE vovoltar;
+
+select * from empresas;
+select * from usuariospf;
+select * from vagas;
 
 CREATE TABLE usuariospf (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     telefone VARCHAR(255) NOT NULL,
-    nascimento varchar(255) NOT NULL,
+    nascimento VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
     ft_perfil VARCHAR(255),
-    curriculo varchar(255)
+	area_atuacao VARCHAR(255),
+    curriculo VARCHAR(255)
 );
 
+drop table vagas;
+	
 CREATE TABLE empresas (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
@@ -29,10 +35,7 @@ create table vagas (
     email_empresa varchar(255) not null,
     cidade varchar(255) not null,
     estado varchar(255) not null,
-    qtd_vagas varchar(100) not null
+    qtd_vagas varchar(100) not null,
+	empresa_id INT,
+    CONSTRAINT fk_empresa_vaga FOREIGN KEY (empresa_id) REFERENCES empresas(id)
 );
-
-select * from empresas;
-select * from vagas;
-select * from usuariospf;
-drop table usuariospf;
